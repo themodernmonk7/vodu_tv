1ff4fe52e5fe30c9d9bd26eacf02b52121a80573
themodernmonk7 <sauravavishek@gmail.com>
2022-10-07 18:31:43 +0530

    FIX: Enviroment variable bug

--- src/contex/contex.jsx
+++ src/contex/contex.jsx
@@ -1,10 +1,28 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=78d635b0`
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${
+  import.meta.env.VITE_MOVIE_API_KEY
+}`
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true)
+  const [movies, setMovies] = useState([])
+  const [error, setError] = useState(false)
+  const [query, setQuery] = useState("batman")
+
+  // Fetch movies
+  const fetchMovies = async (url) => {
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`)
+  }, [query])
+
   return <AppContext.Provider value="value">{children}</AppContext.Provider>
 }
 
